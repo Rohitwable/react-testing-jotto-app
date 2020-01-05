b548392c92a0aecf41deab98cfba8ebef609e30a
Rohitwable <rohitwable46@gmail.com>
2020-01-05 13:47:37 +0530

    moved enzyme configuration to setupTests

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from './testUtils';
 
-Enzyme.configure({ adapter: new Adapter() });
 
 /**
  * @function setupComponent
