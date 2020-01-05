2241331c1636aa7651d9b3b52996ae28cff53426
Rohitwable <rohitwable46@gmail.com>
2020-01-05 17:59:17 +0530

    redux config

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - array of guessed words
+ * @param {object} action
+ * @returns {boolean} new success state
+ */
+export default ( state, action)=>{
+    return null;
+}
